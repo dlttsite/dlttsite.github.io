7446996221483cdb6aa1360adfd39fcd543d6d7f
dlttsite <dltt@site>
2020-08-02 17:33:16 +0800

    +

--- js/head.js
+++ js/head.js
@@ -1,5 +1,5 @@
 var lang = navigator.language.toLocaleLowerCase();
 var langs = ['zh-cn', 'zh-hans-cn', 'zh-hans'];
 if (langs.indexOf(lang) > -1) {
-    location.href='error.html'
+    document.getElementsByTagName("body")[0].innerHTML = "error";
 }
