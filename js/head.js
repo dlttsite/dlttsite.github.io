e6dc3e8c593e7abcdf24c38f84e25ca9b7abda2b
dlttsite <dltt@site>
2020-08-02 21:57:33 +0800

    -

--- js/head.js
+++
@@ -1,7 +0,0 @@
-var lang = navigator.language.toLocaleLowerCase();
-var langs = ['zh-cn', 'zh-hans-cn', 'zh-hans'];
-if (langs.indexOf(lang) > -1) {
-    document.addEventListener("DOMContentLoaded", function() {
-      document.getElementsByTagName("body")[0].innerHTML = "error";
-    });
-}
