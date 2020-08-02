6898b3fd3078880eebba535b2c45ac3ee6f6e3c8
dlttsite <dltt@site>
2020-08-02 17:40:34 +0800

    +

--- js/head.js
+++ js/head.js
@@ -1,5 +1,7 @@
 var lang = navigator.language.toLocaleLowerCase();
 var langs = ['zh-cn', 'zh-hans-cn', 'zh-hans'];
 if (langs.indexOf(lang) > -1) {
-    document.getElementsByTagName("body")[0].innerHTML = "error";
+    document.addEventListener("DOMContentLoaded", function() {
+      document.getElementsByTagName("body")[0].innerHTML = "error";
+    });
 }
