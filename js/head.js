703f8eab300ee26d5cd0463863dba21efb258b1e
dlttsite <dltt@site>
2020-08-04 19:55:09 +0800

    +theme

--- js/head.js
+++ js/head.js
@@ -3,5 +3,6 @@ var langs = ['zh-cn', 'zh-hans-cn', 'zh-hans'];
 if (langs.indexOf(lang) > -1) {
     document.addEventListener("DOMContentLoaded", function() {
       document.getElementsByTagName("body")[0].innerHTML = "error";
+      document.title = 'error';
     });
 }
